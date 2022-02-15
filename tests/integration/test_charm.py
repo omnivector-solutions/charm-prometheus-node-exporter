eb7e0791ec1f21094bd1f88262a4af18d8830b2d
Pietro Pasotti <starfire.daemon@gmail.com>
2022-02-15 11:53:58 +0100

    deployment test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -1,13 +1,40 @@
 from xml.etree.ElementPath import ops
 import pytest
 
+from pytest_operator.plugin import OpsTest
+
+
 @pytest.mark.abort_on_fail
-async def test_deploy(prometheus_node_exporter_charm, ops_test):
-    await ops_test.model.deploy('prometheus2')
-    await ops_test.model.deploy(prometheus_node_exporter_charm)
+async def test_deploy(prometheus_node_exporter_charm, ops_test: OpsTest):
+    await ops_test.model.deploy('ubuntu', num_units=3)
+    await ops_test.model.deploy('telegraf', num_units=0)
     
-    await ops_test.model.relate()
-    await ops_test.model.wait_for_idle()
+    # subordinate charms
+    await ops_test.model.add_relation(
+        'telegraf:juju-info', 
+        'ubuntu:juju-info'
+    )
+    await ops_test.model.wait_for_idle(wait_for_active=True)
+    
+    
+    await ops_test.model.deploy(prometheus_node_exporter_charm, num_units=0)
+    await ops_test.model.add_relation(
+        'prometheus-node-exporter:juju-info',
+        'ubuntu:juju-info'
+    )
+    await ops_test.model.wait_for_idle(wait_for_active=True)
+
+    # prom2
+    await ops_test.model.deploy('prometheus2')
+    await ops_test.model.add_relation(
+        'prometheus2:target',
+        'telegraf:prometheus-client'
+    )
+    await ops_test.model.add_relation(
+        'prometheus2:scrape',
+        'prometheus-node-exporter:prometheus'
+    )
+    await ops_test.model.wait_for_idle(wait_for_active=True)
     
     
 async def test_status(ops_test):
